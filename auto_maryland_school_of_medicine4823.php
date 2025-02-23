c5f5d2cdfe2e0f4e12da565d0833f580610c7b3c
Gautam Kumar <152634935+GoutamArya15@users.noreply.github.com>
2025-02-23 23:19:28 +0530

    Update auto_maryland_school_of_medicine4823.php

--- auto_maryland_school_of_medicine4823.php
+++ auto_maryland_school_of_medicine4823.php
@@ -1,18 +1,28 @@
-<?php
-//
+[6:41 PM, 2/22/2025] Himanshi Kurukshetra: <?php
 error_reporting(E_ALL);
-include_once("/xampp/htdocs/simple-html-dom/simplehtmldom_1_9_1/simple_html_dom.php");
+$root = dirname(_FILE_) . "/../../../../../";
+$include_path = $root . "/lib/include/";
+set_include_path($include_path);
+include_once("htmlMimeMail.php");
+include_once("DB.config.class.inc");
+include("Query.class.inc");
+include("misc.inc");
+include_once("simple_html_dom.php");
+$env = "test";
+$inst_name = "Maryland School of Medicine";
 $scrap_url = "https://www.medschool.umaryland.edu/news/2025/";
 $html = file_get_html($scrap_url);
-$array = [];
-
+$arry = [];
+$title1 = $subtitle1 = $content = $image1 = $video1 = $text1 = $contact1 = $tags1 = $img = $headline = '';
 $content = $html->find('div.content-wrapper div.inner-wrapper div.media');
+$cdate = date('Y-m-d', strtotime(' -15 day'));
+
 foreach ($content as $item) {
     $date = '';
     $heading = '';
-    $description = '';
+    $desc = '';
     $sub_heading = '';
-    $text = '';
+    $text1 = '';
     $date1 = $item->find("div.bd p.h4", 0);
     $date1 = $date1 ? trim($date1->innertext) : '';
     if (strpos($date1, ',') !== false) {
@@ -24,24 +34,39 @@ foreach ($content as $item) {
     } else {
         $date = '0000-00-00';
     }
-    $description = $item->find("div.bd p", 1);
-    $description = $description ? $description->innertext : '';
+	if ($date >= $cdate) {
+    $desc1 = $item->find("div.bd p", 1);
+    $desc = $desc1 ? $desc1->innertext : '';
     $headingTag = $item->find("div.bd h3.h4 a", 0);
     $heading = $headingTag ? $headingTag->innertext : '';
     $href = $headingTag ? $headingTag->href : '';
     $full_url = strpos($href, 'http') === 0 ? $href : "https://www.medschool.umaryland.edu/" . $href;
     $html1 = file_get_html($full_url);
-
     if ($html1) {
-        $img = $html1->find('div.alt-to-caption p');
+        $alt = $html1->find('section.silk-tabs__wrapper', 0);
+        $sub =  $html1->find('div.alt-to-caption h3', 1);
+        $sub_heading = $sub->plaintext;
+        $main_paragraph = $html1->find('div.alt-to-caption p,h3');
+        $alt_to_caption = $html1->find('div.alt-to-caption', 0);
         $images = [];
         $video = [];
-        foreach ($img as $figure) {
-            $content = $figure->outertext;
-            $data =  preg_replace('/<img.*?>/i', '', $content);
-            $text = $text . $data;
+        foreach ($alt_to_caption as $val) {
+            $content = $val->outertext;
+            $patterns = [
+                '/<img\b[^>]*>/i',                                   // Remove <img> tags
+                '/<h3\b[^>]>.?<\/h3>/is',
+                '/<h2\b[^>]>.?<\/h2>/is',
+                '/<section\b[^>]>.?<\/section>/is',
+                '/<iframe\b[^>]>.?<\/iframe>/is',                         // Remove <h3> tags and their content
+                '/<div[^>]\bclass=["\'][^"\']*silk-tabs[^"\']["\'][^>]>.?<\/div>/is', // Remove <div> elements with class "silk-tabs"
+            ];
+            $result = preg_replace($patterns, '', $content);
+            $text1 .= $result;
+        }
+
+
+        foreach ($main_paragraph as $figure) {
             $imgTag = $figure->find('img', 0);
-            $iframe = $figure->find('div.video-responsive', 0);
             $captionTag = $figure->find('img[alt]', 0);
             $imageSrc = $imgTag ? $imgTag->src : null;
             $captionText = $captionTag ? $captionTag->getAttribute('alt') : '';
@@ -52,30 +77,27 @@ foreach ($content as $item) {
                     'caption' => $captionText,
                 ];
             }
-            if (!empty($iframe_src) && !in_array(['video' => $iframe_src], $video) && count($video) > 0) {
-                $video[] = [
-                    'video' => $iframe_src,
-                ];
-            } else {
-                $video = '';
-            }
         }
-        $array[] = [
-            'date' => $date,
-            'heading' => $heading,
-            'description' => $description,
-            'sub-heading' => '',
-            'image' => $images,
-            'content' => $text,
-            'video' => $video,
+
+        $arry[] = [
+			'title' => $heading,
+            'url' =>$full_url,
+            'date' =>$date ,
+            'description' => strip_tags($desc).'...',
+            'content' =>$text1,
+            'doiName' => "",
+            'doiLink' => "",
+            'pubWebsite' =>'',
+            'gscholer' => "",
+            'img'=>$images , 
+            'citationLink' => '',
+            'subhead'=>$sub_heading,   
+            'caption'=>''
         ];
     }
 }
-
+}
+	
 echo "<pre>";
-print_r($array);
-echo "</pre>";
-
-
-
-
+var_dump($arry);
+die('sfdf');
